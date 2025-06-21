158dc63129177e7802c42a914c59c184caee282e
DFCH9503 <dafechaparro@hotmail.com>
2025-06-21 13:30:56 -0500

    soccer-players databases

---
+++ databases/soccer-players/soccer-players.sql
@@ -0,0 +1,11 @@
+BEGIN
+    SELECT 
+        GROUP_CONCAT(
+            CONCAT_WS(' ', first_name, surname, CONCAT(
+                "#", player_number
+            ))
+            ORDER BY player_number SEPARATOR ';'
+        ) AS players
+    FROM soccer_team
+    ORDER BY player_number;
+END
