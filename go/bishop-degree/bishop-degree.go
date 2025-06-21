e3fe148461aa218d9d65477d8deffee502bb7b57
DFCH9503 <dafechaparro@hotmail.com>
2025-06-21 13:14:00 -0500

    bishp-degree go

---
+++ go/bishop-degree/bishop-degree.go
@@ -0,0 +1,22 @@
+package main
+
+import(
+	"fmt"
+)
+
+func bishopAndPawn(bishop, pawn string) bool{
+	bishopX, bishopY := rune(bishop[0]), rune(bishop[1])
+	pawnX, pawnY := rune(pawn[0]), rune(pawn[1])
+	
+	if bishopX + pawnY == bishopY + pawnX || bishopX + bishopY == pawnX + pawnY {
+    	return true
+	}
+
+return false
+}
+
+func main(){
+	bishop, pawn := "a1", "c3"
+
+	fmt.Printf("A bishop placed in the square %s of an astandard chess board  can capture a pawn placed in the square %s: %v", bishop, pawn,  bishopAndPawn(bishop, pawn))
+}
